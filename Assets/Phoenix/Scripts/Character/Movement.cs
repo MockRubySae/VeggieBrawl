992bfdd0ed3aae921cadeac998f3a0fad508b251
SAE1\1030789 <1030789@student.sae.edu.au>
2024-02-19 13:34:21 +1000

    build working

--- Assets/Phoenix/Scripts/Character/Movement.cs
+++ Assets/Phoenix/Scripts/Character/Movement.cs
@@ -1,7 +1,3 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
