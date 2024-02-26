1d79b78736868b19c9180f16f121664ab4982467
SAE1\1031438 <1031438@student.sae.edu.au>
2024-02-26 13:51:39 +1000

    Custom font added, point system expanded, added highscores

--- Assets/Phoenix/Scripts/Enemies/GarlicEnemy.cs
+++ Assets/Phoenix/Scripts/Enemies/GarlicEnemy.cs
@@ -76,7 +76,7 @@ public class GarlicEnemy : MonoBehaviour
         {
             Instantiate(garlicDrop, transform.position, transform.rotation);
         }
-        ScoreManager.instance.EnemyAddPoint();
+        ScoreManager.instance.EnemyAddPoint(100);
         yield return new WaitForSeconds(1.5f);
         Destroy(gameObject);
     }
--- Assets/Phoenix/Scripts/PickUps/GarlicPickUp.cs
+++ Assets/Phoenix/Scripts/PickUps/GarlicPickUp.cs
@@ -51,6 +51,7 @@ public class GarlicPickUp : MonoBehaviour
         player.speed = 0f;
         yield return new WaitForSeconds(1);
         garlic.GarlicGain(25);
+        ScoreManager.instance.EnemyAddPoint(500);
         player.speed = 10f;
         Destroy(gameObject);
         Debug.Log("finished eatingt");
