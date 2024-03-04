29a12db27fbc523a7e3d5b2222089f29d867e450
SAE1\1030021 <1030021@student.sae.edu.au>
2024-03-04 13:26:23 +1000

    irradiated tiles added

--- Assets/Nathan/Script/TilePainter.cs
+++ Assets/Nathan/Script/TilePainter.cs
@@ -6,6 +6,7 @@ using UnityEngine.Tilemaps;
 public class TilePainter : MonoBehaviour
 {
     public Tile grass1, grass2, grass3, grass4;
+    public Tile irrGrass1, irrGrass2, irrGrass3, irrGrass4;
     public Tilemap tilemap;
     public Vector3Int position;
     public int choose;
@@ -13,48 +14,92 @@ public class TilePainter : MonoBehaviour
     public int map;
 
     public Tile farmlandTopLeft, farmlandTopRight, farmlandBottomLeft, farmlandBottomRight, farmlandMiddle, farmlandLeft, farmlandRight, farmlandTop, farmlandBottom;
+    public Tile irrFarmTopLeft, irrFarmTopRight, irrFarmBottomLeft, irrFarmBottomRight, irrFarmMiddle, irrFarmLeft, irrFarmRight, irrFarmTop, irrFarmBottom;
     void Start()
     {
-
-
-        for (int x = -250; x < 250; x++)
+        if (Random.Range(0, 2) == 1)
         {
-            for (int y = -250; y < 250; y++)
+            for (int x = -250; x < 250; x++)
             {
-                position = new Vector3Int(x, y, 0);
-                choose = Random.Range(0, 4);
-                if (choose == 1)
+                for (int y = -250; y < 250; y++)
                 {
-                    paint = grass1;
-                } 
-                else if (choose == 2)
-                {
-                    paint = grass2;
-                } 
-                else if (choose == 3)
-                {
-                    paint = grass3;
+                    position = new Vector3Int(x, y, 0);
+                    choose = Random.Range(0, 4);
+                    if (choose == 1)
+                    {
+                        paint = grass1;
+                    }
+                    else if (choose == 2)
+                    {
+                        paint = grass2;
+                    }
+                    else if (choose == 3)
+                    {
+                        paint = grass3;
+                    }
+                    else
+                    {
+                        paint = grass4;
+                    }
+                    tilemap.SetTile(position, paint);
                 }
-                else
-                {
-                    paint = grass4;
-                }
-                tilemap.SetTile(position, paint);
+
             }
-            
+
+            for (int x = -250; x < 250; x++)
+            {
+                for (int y = -250; y < 250; y++)
+                {
+                    position = new Vector3Int(x, y, 0);
+                    if (Random.Range(0, 2501) == 2500)
+                    {
+                        PaintFarmland();
+                    }
+                }
+            }
+        } 
+        else
+        {
+            for (int x = -250; x < 250; x++)
+            {
+                for (int y = -250; y < 250; y++)
+                {
+                    position = new Vector3Int(x, y, 0);
+                    choose = Random.Range(0, 4);
+                    if (choose == 1)
+                    {
+                        paint = irrGrass1;
+                    }
+                    else if (choose == 2)
+                    {
+                        paint = irrGrass2;
+                    }
+                    else if (choose == 3)
+                    {
+                        paint = irrGrass3;
+                    }
+                    else
+                    {
+                        paint = irrGrass4;
+                    }
+                    tilemap.SetTile(position, paint);
+                }
+
+            }
+
+            for (int x = -250; x < 250; x++)
+            {
+                for (int y = -250; y < 250; y++)
+                {
+                    position = new Vector3Int(x, y, 0);
+                    if (Random.Range(0, 2501) == 2500)
+                    {
+                        PaintIrradiatedFarm();
+                    }
+                }
+            }
         }
 
-        for (int x = -250; x < 250; x++)
-        {
-            for (int y = -250; y < 250; y++)
-            {
-                position = new Vector3Int(x, y, 0);
-                if (Random.Range(0,2501) == 2500)
-                {
-                    PaintFarmland();
-                }
-            }
-        }
     }
 
     public void PaintFarmland()
@@ -81,6 +126,34 @@ public class TilePainter : MonoBehaviour
             for (int j = 1; j < 3; j++)
             {
                 tilemap.SetTile(new Vector3Int(position.x + i, position.y - j, 0), farmlandMiddle);
+            }
+        }
+    }
+
+    public void PaintIrradiatedFarm()
+    {
+        tilemap.SetTile(position, irrFarmTopLeft);
+        tilemap.SetTile(new Vector3Int(position.x, position.y - 3, 0), irrFarmBottomLeft);
+        tilemap.SetTile(new Vector3Int(position.x + 6, position.y, 0), irrFarmTopRight);
+        tilemap.SetTile(new Vector3Int(position.x + 6, position.y - 3, 0), irrFarmBottomRight);
+
+        for (int i = 1; i < 3; i++)
+        {
+            tilemap.SetTile(new Vector3Int(position.x, position.y - i, 0), irrFarmLeft);
+            tilemap.SetTile(new Vector3Int(position.x + 6, position.y - i, 0), irrFarmRight);
+        }
+
+        for (int i = 1; i < 6; i++)
+        {
+            tilemap.SetTile(new Vector3Int(position.x + i, position.y, 0), irrFarmTop);
+            tilemap.SetTile(new Vector3Int(position.x + i, position.y - 3, 0), irrFarmBottom);
+        }
+
+        for (int i = 1; i < 6; i++)
+        {
+            for (int j = 1; j < 3; j++)
+            {
+                tilemap.SetTile(new Vector3Int(position.x + i, position.y - j, 0), irrFarmMiddle);
             }
         }
     }
